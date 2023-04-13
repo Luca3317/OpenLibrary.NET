c064ef99027d8ba5fa3ace73a453ac75527eed48
Luca3317 <lweist3317@gmail.com>
2023-04-13 19:28:24 +0200

    Added missing Try methods in OLLoaders

--- src/Utility/OLLoader/OLListLoader.cs
+++ src/Utility/OLLoader/OLListLoader.cs
@@ -13,15 +13,27 @@ namespace OpenLibraryNET
 
         HttpClient _client;
 
+        public async Task<(bool, OLListData[]?)> TryGetUsersListsAsync(string username)
+            => await TryGetUsersListAsync(_client, username);
         public async Task<OLListData[]?> GetUserListsAsync(string username)
             => await GetUserListsAsync(_client, username);
 
+        public async Task<(bool, OLListData?)> TryGetListAsync(string username, string id)
+            => await TryGetListAsync(_client, username, id);
         public async Task<OLListData?> GetListAsync(string username, string id)
             => await GetListAsync(_client, username, id);
 
+        public async Task<(bool, OLEditionData[]?)> TryGetListEditionsAsync(string username, string id, params KeyValuePair<string, string>[] parameters)
+            => await TryGetListEditionsAsync(_client, username, id, parameters);
         public async Task<OLEditionData[]?> GetListEditionsAsync(string username, string id, params KeyValuePair<string, string>[] parameters)
             => await GetListEditionsAsync(_client, username, id, parameters);
 
+
+        public async static Task<(bool, OLListData[]?)> TryGetUsersListAsync(HttpClient client, string username)
+        {
+            try { return (true, await GetUserListsAsync(client, username)); }
+            catch { return (false, null); }
+        }
         public async static Task<OLListData[]?> GetUserListsAsync(HttpClient client, string username)
         {
             return await OpenLibraryUtility.LoadAsync<OLListData[]>
@@ -32,6 +44,11 @@ namespace OpenLibraryNET
             );
         }
 
+        public async static Task<(bool, OLListData?)> TryGetListAsync(HttpClient client, string username, string id)
+        {
+            try { return (true, await GetListAsync(client, username, id)); }
+            catch { return (false, null); }
+        }
         public async static Task<OLListData?> GetListAsync(HttpClient client, string username, string id)
         {
             return await OpenLibraryUtility.LoadAsync<OLListData>
@@ -41,6 +58,11 @@ namespace OpenLibraryNET
             );
         }
 
+        public async static Task<(bool, OLEditionData[]?)> TryGetListEditionsAsync(HttpClient client, string username, string id, params KeyValuePair<string, string>[] parameters)
+        {
+            try { return (true, await GetListEditionsAsync(client, username, id, parameters)); }
+            catch { return (false, null); }
+        }
         public async static Task<OLEditionData[]?> GetListEditionsAsync(HttpClient client, string username, string id, params KeyValuePair<string, string>[] parameters)
         {
             return await OpenLibraryUtility.LoadAsync<OLEditionData[]>
--- src/Utility/OLLoader/OLRecentChangesLoader.cs
+++ src/Utility/OLLoader/OLRecentChangesLoader.cs
@@ -13,33 +13,61 @@ namespace OpenLibraryNET
 
         HttpClient _client;
 
+        public async Task<(bool, OLRecentChangesData[]?)> TryGetRecentChangesAsync(string kind = "", params KeyValuePair<string, string>[] parameters)
+            => await TryGetRecentChangesAsync(_client, kind, parameters);
         public async Task<OLRecentChangesData[]?> GetRecentChangesAsync(string kind = "", params KeyValuePair<string, string>[] parameters)
             => await GetRecentChangesAsync(_client, kind, parameters);
 
+        public async Task<(bool, OLRecentChangesData[]?)> TryGetRecentChangesAsync(string year, string kind = "", params KeyValuePair<string, string>[] parameters)
+            => await TryGetRecentChangesAsync(_client, year, kind, parameters);
         public async Task<OLRecentChangesData[]?> GetRecentChangesAsync(string year, string kind = "", params KeyValuePair<string, string>[] parameters)
             => await GetRecentChangesAsync(_client, year, kind, parameters);
 
+        public async Task<(bool, OLRecentChangesData[]?)> TryGetRecentChangesAsync(string year, string month, string kind = "", params KeyValuePair<string, string>[] parameters)
+            => await TryGetRecentChangesAsync(_client, year, month, kind, parameters);
         public async Task<OLRecentChangesData[]?> GetRecentChangesAsync(string year, string month, string kind = "", params KeyValuePair<string, string>[] parameters)
             => await GetRecentChangesAsync(_client, year, month, kind, parameters);
 
+        public async Task<(bool, OLRecentChangesData[]?)> TryGetRecentChangesAsync(string year, string month, string day, string kind = "", params KeyValuePair<string, string>[] parameters)
+            => await TryGetRecentChangesAsync(_client, year, month, day, kind, parameters);
         public async Task<OLRecentChangesData[]?> GetRecentChangesAsync(string year, string month, string day, string kind = "", params KeyValuePair<string, string>[] parameters)
             => await GetRecentChangesAsync(_client, year, month, day, kind, parameters);
 
+        public async static Task<(bool, OLRecentChangesData[]?)> TryGetRecentChangesAsync(HttpClient client, string kind = "", params KeyValuePair<string, string>[] parameters)
+        {
+            try { return (true, await GetRecentChangesAsync(client, kind, parameters)); }
+            catch { return (false, null); }
+        }
         public async static Task<OLRecentChangesData[]?> GetRecentChangesAsync(HttpClient client, string kind = "", params KeyValuePair<string, string>[] parameters)
         {
             return await OpenLibraryUtility.LoadAsync<OLRecentChangesData[]>(OpenLibraryUtility.BuildRecentChangesURL(kind, parameters), client: client);
         }
 
+        public async static Task<(bool, OLRecentChangesData[]?)> TryGetRecentChangesAsync(HttpClient client, string year, string kind = "", params KeyValuePair<string, string>[] parameters)
+        {
+            try { return (true, await GetRecentChangesAsync(client, year, kind, parameters)); }
+            catch { return (false, null); }
+        }
         public async static Task<OLRecentChangesData[]?> GetRecentChangesAsync(HttpClient client, string year, string kind = "", params KeyValuePair<string, string>[] parameters)
         {
             return await OpenLibraryUtility.LoadAsync<OLRecentChangesData[]>(OpenLibraryUtility.BuildRecentChangesURL(year, kind, parameters), client: client);
         }
 
+        public async static Task<(bool, OLRecentChangesData[]?)> TryGetRecentChangesAsync(HttpClient client, string year, string month, string kind = "", params KeyValuePair<string, string>[] parameters)
+        {
+            try { return (true, await GetRecentChangesAsync(client, year, month, kind, parameters)); }
+            catch { return (false, null); }
+        }
         public async static Task<OLRecentChangesData[]?> GetRecentChangesAsync(HttpClient client, string year, string month, string kind = "", params KeyValuePair<string, string>[] parameters)
         {
             return await OpenLibraryUtility.LoadAsync<OLRecentChangesData[]>(OpenLibraryUtility.BuildRecentChangesURL(year, month, kind, parameters), client: client);
         }
 
+        public async static Task<(bool, OLRecentChangesData[]?)> TryGetRecentChangesAsync(HttpClient client, string year, string month, string day, string kind = "", params KeyValuePair<string, string>[] parameters)
+        {
+            try { return (true, await GetRecentChangesAsync(client, year, month, day, kind, parameters)); }
+            catch { return (false, null); }
+        }
         public async static Task<OLRecentChangesData[]?> GetRecentChangesAsync(HttpClient client, string year, string month, string day, string kind = "", params KeyValuePair<string, string>[] parameters)
         {
             return await OpenLibraryUtility.LoadAsync<OLRecentChangesData[]>(OpenLibraryUtility.BuildRecentChangesURL(year, month, day, kind, parameters), client: client);
